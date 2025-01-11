0eb5a9db8a1a7756aed7d3e2a2fb3f0f256db2ac
Upendra Sahu <upendrasahu1199@gmail.com>
2025-01-11 22:05:50 +0530

    Added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import qustionsArr from '../data/algebraQuestions.js'
+import prepSathiLogo from '../assets/prep_sathi_logo_white_transparent.png'
 
 const Navbar =  () => {
   return (
     <div className="navbar bg-base-300 mb-5">
-      <button className="btn btn-ghost text-xl">Prep Sathi</button>
+      <button className="btn btn-ghost text-xl"> <img src={prepSathiLogo} alt="" className='h-1/2' />Prep Sathi</button>
     </div>
   )
 }
