a36bf9592556eeb224047d92e47782d2555124a0
Upendra Sahu <upendrasahu1199@gmail.com>
2025-01-12 19:29:21 +0530

    Try to add some navbar feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import prepSathiLogo from '../assets/prep_sathi_logo_white_transparent.png'
 const Navbar =  () => {
   return (
     <div className="navbar bg-base-300 mb-5 fixed top-0 z-49">
-      <button className="btn btn-ghost text-xl"> <img src={prepSathiLogo} alt="" className='h-1/2' />Prep Sathi</button>
+      <button className="btn btn-ghost text-xl"><img src={prepSathiLogo} alt="" className='h-1/2' />Prep Sathi</button>
     </div>
   )
 }
